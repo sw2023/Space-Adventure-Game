016acd4860ddd92e3b6b58fc52417ea1eb9ba52b
sw2023 <spudbob168@gmail.com>
2019-07-10 18:56:45 -0700

    Update Sprites.py
    Implement complex buffered drawing to optimize render speeds

--- Sprites.py
+++ Sprites.py
@@ -1,11 +1,27 @@
 import arcade
+import timeit
 
-SCREEN_WIDTH = 1000
+SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
 SCREEN_TITLE = "Space Adventure"
 
-def draw_background():
-    pass
+HALF_SQUARE_WIDTH = 2.5
+HALF_SQUARE_HEIGHT = 2.5
+SQUARE_SPACING = 10
 
-def spaceship():
-    pass
+class MyGame(arcade.Window):
+    # Main application class
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        arcade.set_background_color(arcade.color.BLACK)
+
+        self.draw_time = 0
+        self.shape_list = None
+    
+    def setup(self):
+        self.shape_list = arcade.ShapeElementList()
+
+        point_list = []
+        color_list = []
