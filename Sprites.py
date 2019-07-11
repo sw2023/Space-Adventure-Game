40702b60e1351c4d20115abe15520523dffae2f1
sw2023 <spudbob168@gmail.com>
2019-07-10 19:11:46 -0700

    Update Sprites.py

--- Sprites.py
+++ Sprites.py
@@ -21,7 +21,31 @@ class MyGame(arcade.Window):
         self.shape_list = None
     
     def setup(self):
+        # Credit for complex shape list code: http://arcade.academy/examples/shape_list_demo.html
+
         self.shape_list = arcade.ShapeElementList()
 
         point_list = []
         color_list = []
+
+        for x in range(0, SCREEN_WIDTH, SQUARE_SPACING):
+            for y in range(0, SCREEN_HEIGHT, SQUARE_SPACING):
+
+                # calculate 4 points of the rectangle if x and y are the center coords
+                top_left_corner = (x - HALF_SQUARE_WIDTH, y + HALF_SQUARE_HEIGHT)
+                top_right_corner = (x + HALF_SQUARE_WIDTH, y + HALF_SQUARE_HEIGHT)
+                bottom_right_corner = (x + HALF_SQUARE_WIDTH, y - HALF_SQUARE_HEIGHT)
+                bottom_left_corner = (x - HALF_SQUARE_WIDTH, y - HALF_SQUARE_HEIGHT)
+
+                # Add points to point_list
+                point_list.append(top_left_corner)
+                point_list.append(top_right_corner)
+                point_list.append(bottom_right_corner)
+                point_list.append(bottom_left_corner)
+
+                # add color to each point
+                for i in range(4):
+                    color_list.append(arcade.color.BLACK)
+        
+        shape = arcade.create_rectangles_filled_with_colors(point_list, color_list)
+        self.shape_list.append(shape)
